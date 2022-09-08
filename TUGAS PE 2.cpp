e7f11e2166e0fe6686b59bf6d4dd94b955fadad1
Yzrofiq26 <112595183+Yzrofiq26@users.noreply.github.com>
2022-09-08 10:20:46 +0700

    Add files via upload

---
+++ TUGAS PE 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,p,l;
+	cout<<"PROGRAM C++ MEHITUNG LUAS PERSEGI PANJANG";
+	
+	cout<<"___________________________________"<<endl;
+	cout<<"PANJANG * LEBAR"; 
+	cin>>p;
+	cout<<"4*4 ";
+	cin>>l;
+	a=4*4;
+	cout<<"HASIL: "<<a<< endl;
+	return 0;
+}
